da5742af6bd3c97bb0cc5684ea7b571d5affe756
Ramcharxn <ramcharxn@gmail.com>
2021-02-11 20:27:25 +0530

    Books created

---
+++ routes/books.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const Author = require('../models/author')
+const Book = require('../models/book')
+const router = express.Router()
+const path = require('path')
+const uploadPath = path.join('public',Book.coverImageBasePath)
+const multer = require('multer')
+const fs = require('fs')
+const imageMimeTypes = ['image/jpeg','image/png','images/gif']
+const upload = multer({
+    dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype))
+    }
+})
+
+// All Book
+router.get('/',async(req,res) => {
+    let query = Book.find()
+    if(req.query.title != null && req.query.title != ''){
+        query = query.regex('title',new RegExp(req.query.title,'i'))
+    }
+    if(req.query.publisedBefore != null && req.query.publisedBefore != ''){
+        query = query.ltr('publishDate',req.query.publisedBefore)
+    }
+    if(req.query.publisedAfter != null && req.query.publisedAfter != ''){
+        query = query.gtr('publishDate',req.query.publisedAfter)
+    }
+    try {
+        const books = await query.exec()
+        res.render('books/index',{
+            books:books,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+// New Book
+router.get('/new',async(req,res) => {
+   renderNewPage(res, new Book())
+})
+
+// Create Book
+router.post('/',upload.single('cover'), async(req,res) => {
+    const fileName = req.file != null ? req.file.filename : null
+    const book = new Book({
+        title:req.body.title,
+        author:req.body.author,
+        publishDate: new Date(req.body.publishDate),
+        pageCount:req.body.pageCount,
+        coverImageName: fileName,
+        description:req.body.description
+    })
+    try {
+        const newBook = await book.save()
+        res.redirect('books')
+    } catch {
+        if(book.coverImageName != null){
+            removeBookCover(book.coverImageName)
+        }
+        renderNewPage(res, book, true)
+    }
+})
+
+function removeBookCover(fileName){
+    fs.unlink(path.join(uploadPath,fileName),err => {
+        if(err) console.error(err);
+    })
+}
+
+async function renderNewPage(res, book, hasError = false) {
+    try {
+        const authors = await Author.find({})
+        // const book = new Book()
+        const params = {
+            authors: authors,
+            book: book
+        }
+        if(hasError) params.errorMessage = 'Error Creating Book'
+        res.render('books/new', params)
+    } catch {
+        res.redirect('/books')
+    }
+}
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
  
 const app = express()
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit:'10mb', extended:false }))
 
 const mongoose = require('mongoose')
 const mongoURI = 'mongodb+srv://shop2:shop2@shop2.c7npm.mongodb.net/shop2?retryWrites=true&w=majority'
@@ -21,5 +25,7 @@ db.on('error',error => console.error(error))
 db.once('open',() => console.log('connected'))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 app.listen(process.env.PORT || 3002)
